859d5607616cee7b62a3bdd44700a4aea3a41f68
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-04-25 13:03:01 -0500

    vue

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,13 @@
+<script setup>
+let count = 0;
+function addvalue() {
+    count++;
+    console.log(count);
+}
+</script>
+
+<template>
+<div @click="addvalue()" class="bg-blue-500 text-white py-2 px-4 rounded inline-block hover:cursor-pointer hover:bg-blue-700 transition-colors">
+    ¡presioname! {{ count }}
+</div>
+</template>
